fa57ce58bdcf7d241b1f4906a257b7583919332d
Pfhoenix <pfhoenix@gmail.com>
2021-03-14 15:52:09 -0400

    initial commit

---
+++ Plugin.cs
@@ -0,0 +1,31 @@
+﻿using System.Reflection;
+using BepInEx;
+using BepInEx.Logging;
+using HarmonyLib;
+
+namespace $safeprojectname$
+{
+	[BepInPlugin("your.unique.mod.identifier", Plugin.ModName, Plugin.Version)]
+	public class Plugin : BaseUnityPlugin
+	{
+		public const string Version = "1.0";
+		public const string ModName = "My Mod";
+		Harmony _Harmony;
+		public static ManualLogSource Log;
+
+		private void Awake()
+		{
+#if DEBUG
+			Log = Logger;
+#else
+			Log = new ManualLogSource(null);
+#endif
+			_Harmony = Harmony.CreateAndPatchAll(Assembly.GetExecutingAssembly(), null);
+		}
+
+		private void OnDestroy()
+		{
+			if (_Harmony != null) _Harmony.UnpatchAll(null);
+		}
+	}
+}
